59447719e3663f356b418cc30fc7096e5e8ab197
Jan Wille <mail@janwille.de>
2022-08-23 16:09:00 +0200

    refactor: move cli setup into __main__.py

--- src/makeclean/__init__.py
+++ src/makeclean/__init__.py
@@ -2,28 +2,13 @@ from os import PathLike, remove
 from os.path import exists, isdir
 from shutil import rmtree
 
-import click
+from click import echo, style
 
 
 __version__ = "0.0.1-dev0"
 
 
-@click.command()
-@click.help_option("--help", "-h")
-@click.option("--verbose", "-v", is_flag=True, default=False)
-@click.option("--version", "-V", is_flag=True, default=False)
-@click.argument("PATH", nargs=-1, type=click.Path())
-def main(path, verbose, version):
-    """This utility is meant for use in makefiles as the clean target or similar usecases.
-    It takes an arbitray number of PATH objects and removes them if they exists.
-    Folders will be remove recursivly."""
-    if version:
-        print(f"makeclean.py v{__version__}")
-        return
-    clean(path, verbose)
-
-
-def clean(path: list[str|PathLike], verbose=False):
+def clean(path: list[str | PathLike], verbose=False):
     """Removes all given Path-Objects, meaning files are deleted and
     directorys are removed recurlivly.
 
@@ -34,15 +19,15 @@ def clean(path: list[str|PathLike], verbose=False):
     for p in path:
         if not exists(p):
             if verbose:
-                click.echo(f'{click.style("skip", fg="yellow")}: {p}')
+                echo(f'{style("skip", fg="yellow")}: {p}')
             continue
         if isdir(p):
             if not p.endswith("/"):
                 p += "/"
             if verbose:
-                click.echo(f'{click.style("dir", fg="bright_cyan")}:  {p}')
+                echo(f'{style("dir", fg="bright_cyan")}:  {p}')
             rmtree(p)
         else:
             if verbose:
-                click.echo(f'{click.style("file", fg="blue")}: {p}')
+                echo(f'{style("file", fg="blue")}: {p}')
             remove(p)
--- src/makeclean/__main__.py
+++ src/makeclean/__main__.py
@@ -1,5 +1,22 @@
-from . import main
+import click
+
+from . import __version__, clean
+
+
+@click.command()
+@click.help_option("--help", "-h")
+@click.option("--verbose", "-v", is_flag=True, default=False)
+@click.option("--version", "-V", is_flag=True, default=False)
+@click.argument("PATH", nargs=-1, type=click.Path())
+def cli(path, verbose, version):
+    """This utility is meant for use in makefiles as the clean target or similar
+    usecases. It takes an arbitray number of PATH objects and removes them if they
+    exists. Folders will be remove recursivly."""
+    if version:
+        print(f"makeclean.py v{__version__}")
+        return
+    clean(path, verbose)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
